eaa093d96f63980b3757a9d2fca2458b29fa89a8
Healy <hfuess@pro1iaq.com>
2023-07-21 20:04:20 -0600

    Add sorted csv script for multiple strategy trades on one ticker

---
+++ sort_trade_times_and_export_csv.py
@@ -0,0 +1,10 @@
+import sys
+import csv
+
+import file_utilities
+
+trades_file = sys.argv[1]
+
+csv_df = file_utilities.trades_csv_to_df(trades_file)
+csv_df = csv_df.sort_values(['Symb', 'Time'])
+file_utilities.export_trades_df_to_csv(csv_df, trades_file, description='sorted')
